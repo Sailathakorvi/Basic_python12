2cca3f011c00d910d90c62538bce2f21a0461393
Sailathakorvi <korvisailatha4444@gmail.com>
2024-11-21 16:05:12 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,12 @@
+name =input()
+m =int(input())
+if(m>=90):
+    print("A grade")
+elif(80<=m<=89):
+    print("B grade")
+elif(70<=m<=79):
+    print("c grade")           
+elif(60<=m<=69):
+    print("D grade")
+else:
+    print("fail")        
